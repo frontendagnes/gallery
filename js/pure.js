c284e93929075fa684e034cf0e63f7b17b7a4444
Agnieszka Kaminska <agnieszka.kaminska@ksiegarnia.edu.pl>
2021-05-12 11:20:37 +0200

    href binding added

--- js/pure.js
+++ js/pure.js
@@ -1,14 +1,40 @@
-import { images, gallery } from "./variables.js"
+import { images, gallery, galleryZoom } from "./variables.js"
 
-const galleryPure = () => {
-        images.forEach(image => {
-            const img = new Image()
-            const divEl = document.createElement("div")
+const zoomImage = () => {
+    images.forEach(image => {
+        const imgZoom = new Image()
+            imgZoom.src = image.img
+            imgZoom.classList.add("gallery-zoom__image")
+            imgZoom.id = `img-${image.id}`
+
+        const divZoom = document.createElement("div")
+            divZoom.classList.add("gallery-zoom__div")
+
+        divZoom.appendChild(imgZoom)
+        galleryZoom.appendChild(divZoom)
+})
+}
+const miniatureImage = () => {
+    images.forEach(image => {
+        const href = document.createElement('a')
+        href.href = `#img-${image.id}`
+
+        const img = new Image()
             img.src = image.img
+            img.alt = image.img
+            img.title = image.img
             img.classList.add("gallery__image")
-            divEl.appendChild(img)
+
+        const divEl = document.createElement("div")
+            divEl.classList.add("gallery__div")
+
+            href.appendChild(img)
+            divEl.appendChild(href)
             gallery.appendChild(divEl)
     })
 }
-
+const galleryPure = () => {
+    miniatureImage()
+    zoomImage()
+}
 export default galleryPure
